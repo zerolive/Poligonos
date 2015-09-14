77ca420a4f61eb295f3f5e2d4f597570732a1cdd
zerolive <zerolive@gmail.com>
2015-09-14 18:14:18 +0200

    Triangle knows its perimeter

--- polygon.rb
+++ polygon.rb
@@ -3,13 +3,14 @@ class Polygon
 end
 
 class Triangle
+	NUMBER_OF_SIDES = 3
 	def initialize side_a, side_b, side_c
-
+		@total_perimeter = side_a + side_b + side_c
 	end
 	def number_of_sides
-		3
+		NUMBER_OF_SIDES
 	end
 	def perimeter
-		3
+		@total_perimeter
 	end
 end
