f559289b78c54977ed2bde5a39e6f18abb100787
zerolive <zerolive@gmail.com>
2015-09-14 18:45:40 +0200

    Needs to be closed

--- polygon.rb
+++ polygon.rb
@@ -5,6 +5,8 @@ end
 class Triangle
 	NUMBER_OF_SIDES = 3
 	def initialize side_a, side_b, side_c
+		check_positive(side_a, side_b, side_c)
+		check_closed(side_a, side_b, side_c)
 		@total_perimeter = side_a + side_b + side_c
 	end
 	def number_of_sides
@@ -12,5 +14,18 @@ class Triangle
 	end
 	def perimeter
 		@total_perimeter
+	end
+	
+	private
+
+	def check_positive side_a, side_b, side_c
+		if side_a < 1 || side_b < 1 || side_c < 1
+			raise "Negative values not allowed"
+		end
+	end
+	def check_closed side_a, side_b, side_c
+		if side_a >= side_b + side_c || side_b >= side_a + side_c || side_c >= side_a + side_b
+			raise "Short sides can't be shorter than long side or equal"		
+		end
 	end
 end
--- spec/polygon_spec.rb
+++ spec/polygon_spec.rb
@@ -4,13 +4,21 @@ require 'rspec'
 describe "A triangle" do
 
 
-  it "It has three sides " do
+  it "Has three sides " do
   	a_triangle = Triangle.new(1, 1, 1)
   	expect(a_triangle.number_of_sides).to be(3)
   end
-
+  it "Has positive sides" do
+  	expect{Triangle.new(0, 1, 1)}.to raise_error "Negative values not allowed"
+  end
+  it "Needs to be closed" do
+  	expect{Triangle.new(3, 1, 1)}.to raise_error "Short sides can't be shorter than long side or equal"
+  	expect{Triangle.new(1, 3, 1)}.to raise_error "Short sides can't be shorter than long side or equal"
+  	expect{Triangle.new(1, 1, 3)}.to raise_error "Short sides can't be shorter than long side or equal"
+  	expect{Triangle.new(1, 1, 1)}.not_to raise_error
+  end
   it "Knows its perimeter" do
   	expect(Triangle.new(1, 1, 1).perimeter).to be(3)
-  	expect(Triangle.new(1, 2, 1).perimeter).to be(4)
+  	expect(Triangle.new(1, 2, 2).perimeter).to be(5)
   end
 end
