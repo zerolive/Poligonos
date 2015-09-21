2f5945daeb89937514ff69a8a05566ed5d1b724d
zerolive <zerolive@gmail.com>
2015-09-21 16:09:48 +0200

    Creada la clase poligono y puestas todas sus propiedades

--- polygon.rb
+++ polygon.rb
@@ -1,18 +1,9 @@
 class Polygon
-  
-end
-
-class Triangle
 	def self.build *sides
-		return EquilateralTriangle.new(*sides) if is_equilateral(*sides)
-		return IsoscelesTriangle.new(*sides) if is_isosceles(*sides)
-		return ScaleneTriangle.new(*sides) if is_scalene(*sides)
-	end
-
-	def initialize *sides
-		@sides = *sides
+		@sides = sides
 		check_positive_sides
-		check_closed
+		return Triangle.build(*sides) if number_of_sides == 3
+		return Quadrilateral.build(*sides) if number_of_sides == 4
 	end
 
 	def number_of_sides
@@ -21,6 +12,32 @@ class Triangle
 
 	def perimeter
 		return @sides.inject(:+)
+	end
+
+	private
+
+	def self.check_positive_sides 
+		raise "Non-positive sides arent allowed" if there_are_negatives
+	end
+	def self.number_of_sides
+		return @sides.count
+	end
+	def self.there_are_negatives
+		@sides.any? { |side_value| side_value < 1 }
+	end
+
+end
+
+class Triangle < Polygon
+	def self.build *sides
+		return EquilateralTriangle.new(*sides) if is_equilateral(*sides)
+		return IsoscelesTriangle.new(*sides) if is_isosceles(*sides)
+		return ScaleneTriangle.new(*sides) if is_scalene(*sides)
+	end
+
+	def initialize *sides
+		@sides = *sides
+		check_closed
 	end
 
 	def area
@@ -49,14 +66,8 @@ class Triangle
 		return @sides.inject(:+)/2
 	end
 
-	def check_positive_sides 
-		raise "Negative values not allowed" if there_are_negatives
-	end
 	def check_closed
 		raise "Short sides can't be shorter than long side or equal" if sum_of_every_side_is_longer_than_longer_side_twice
-	end
-	def there_are_negatives
-		@sides.any? { |side_value| side_value < 1 }
 	end
 	def sum_of_every_side_is_longer_than_longer_side_twice
 		@sides.inject(:+) < @sides.max * 2
@@ -103,26 +114,14 @@ class Triangle
 	end
 end
 
-class Quadrilateral
+class Quadrilateral < Polygon
 	def self.build *sides
 		return Square.new(*sides) if is_square(*sides)
 		return Rectangle.new(*sides) if is_rectangle(*sides)
 		return Quadrilateral.new(*sides)
 	end
 	def initialize *sides
-		@sides = sides
-		check_positive_sides
-		
-	end
-	def number_of_sides
-		return @sides.count
-	end
-	def check_positive_sides
-		raise "Non-positive sides arent allowed" if there_are_nonpositive_sides
-	end
-
-	def perimeter
-		return sum_of_sides
+		@sides = sides	
 	end
 
 	def self.is_square *sides
@@ -133,7 +132,7 @@ class Quadrilateral
 	end
 
 	def self.is_rectangle *sides
-		check_two_couples_sides_equal(*sides)
+		check_two_couples_of_equal_sides(*sides)
 	end
 	def is_rectangle
 		return false
@@ -152,25 +151,31 @@ class Quadrilateral
 	def self.sides_are_equal *sides
 		sides.uniq.size == 1
 	end
-	def self.check_two_couples_sides_equal *sides
-		count = 0
-		sides_for_count = []
-		sides.each { |side| sides_for_count << side }
-		sides_for_count.each_with_index do |a_side, a_index|
-			sides_for_count.each_with_index do |b_side, b_index|
-				if a_side == b_side && a_index != b_index
-					count += 1
-					sides_for_count.delete(a_side)
-				end
-			end
+	def self.check_two_couples_of_equal_sides *sides
+		@sides_to_count = sides
+		@count = 0
+		cheking_if_every_side_has_equal_sides
+		return @count == 2
+	end
+	def self.cheking_if_every_side_has_equal_sides
+		@sides_to_count.each_with_index do |a_side, a_index|
+			check_if_a_side_its_equal_to_other_sides(a_side, a_index)
 		end
-		return true if count == 2
-		return false if count !=2
+	end
+	def self.check_if_a_side_its_equal_to_other_sides (a_side, a_index)
+		@sides_to_count.each_with_index do |b_side, b_index|
+			checking_if_its_equal_a_side(a_side, b_side, a_index, b_index)
+		end
+	end
+	def self.checking_if_its_equal_a_side (a_side, b_side, a_index, b_index)
+		if a_side == b_side && a_index != b_index
+			@count += 1
+			@sides_to_count.delete(a_side)
+		end
 	end
 	class Square < Quadrilateral
 		def initialize *sides
 			@sides = sides
-			check_positive_sides
 		end
 		def area
 			return calculate_area
@@ -194,7 +199,6 @@ class Quadrilateral
 	class Rectangle < Quadrilateral
 		def initialize *sides
 			@sides = sides
-			check_positive_sides
 		end
 		def area
 			return calculate_area
--- spec/polygon_spec.rb
+++ spec/polygon_spec.rb
@@ -1,73 +1,73 @@
 require './polygon'
 require 'rspec'
 
-describe "A triangle" do
+describe "A Triangle" do
 
 
   it "Has three sides " do
-  	expect(Triangle.build(1, 1, 1).number_of_sides).to be(3)
+  	expect(Polygon.build(1, 1, 1).number_of_sides).to be(3)
   end
-  it "Has positive sides" do
-  	expect{Triangle.build(0, 1, 1)}.to raise_error "Negative values not allowed"
+  it "Has Polygon sides" do
+  	expect{Polygon.build(0, 1, 1)}.to raise_error "Non-positive sides arent allowed"
   end
   it "Needs to be closed" do
-  	expect{Triangle.build(3, 1, 1)}.to raise_error "Short sides can't be shorter than long side or equal"
-  	expect{Triangle.build(1, 3, 1)}.to raise_error "Short sides can't be shorter than long side or equal"
-  	expect{Triangle.build(1, 1, 3)}.to raise_error "Short sides can't be shorter than long side or equal"
-  	expect{Triangle.build(1, 1, 1)}.not_to raise_error
+  	expect{Polygon.build(3, 1, 1)}.to raise_error "Short sides can't be shorter than long side or equal"
+  	expect{Polygon.build(1, 3, 1)}.to raise_error "Short sides can't be shorter than long side or equal"
+  	expect{Polygon.build(1, 1, 3)}.to raise_error "Short sides can't be shorter than long side or equal"
+  	expect{Polygon.build(1, 1, 1)}.not_to raise_error
   end
   it "Knows its perimeter" do
-  	expect(Triangle.build(1, 2, 2).perimeter).to be(5)
-  	expect(Triangle.build(1, 1, 1).perimeter).to be(3)
+  	expect(Polygon.build(1, 2, 2).perimeter).to be(5)
+  	expect(Polygon.build(1, 1, 1).perimeter).to be(3)
   end
   it "Knows how to find area" do
-  	expect(Triangle.build(3, 4, 5).area).to eq(6)
+  	expect(Polygon.build(3, 4, 5).area).to eq(6)
   end
   it "Knows if its equilateral" do
-	expect(Triangle.build(3, 3, 3).equilateral?).to be(true) 
-	expect(Triangle.build(3, 3, 4).equilateral?).to be(false)
-	expect(Triangle.build(3, 4, 5).equilateral?).to be(false)
+	expect(Polygon.build(3, 3, 3).equilateral?).to be(true) 
+	expect(Polygon.build(3, 3, 4).equilateral?).to be(false)
+	expect(Polygon.build(3, 4, 5).equilateral?).to be(false)
   end
   it "Knows if its isosceles" do
-	expect(Triangle.build(3, 3, 3).isosceles?).to be(false)
-	expect(Triangle.build(3, 4, 3).isosceles?).to be(true)
-	expect(Triangle.build(3, 4, 5).isosceles?).to be(false)
+	expect(Polygon.build(3, 3, 3).isosceles?).to be(false)
+	expect(Polygon.build(3, 4, 3).isosceles?).to be(true)
+	expect(Polygon.build(3, 4, 5).isosceles?).to be(false)
   end
   it "Knows if its scalene" do
-	expect(Triangle.build(3, 3, 3).scalene?).to be(false)
-	expect(Triangle.build(3, 4, 3).scalene?).to be(false)
-	expect(Triangle.build(3, 4, 5).scalene?).to be(true)
+	expect(Polygon.build(3, 3, 3).scalene?).to be(false)
+	expect(Polygon.build(3, 4, 3).scalene?).to be(false)
+	expect(Polygon.build(3, 4, 5).scalene?).to be(true)
   end
 end
 
 describe "A Quadrilateral" do
 
 	it "Has four sides" do
-		expect(Quadrilateral.build(1,1,1,1).number_of_sides).to eq(4)
+		expect(Polygon.build(1,1,1,1).number_of_sides).to eq(4)
 	end
-	it "Has positive sides" do
-		expect{Quadrilateral.build(0,1,1,1)}.to raise_error "Non-positive sides arent allowed"
+	it "Has Polygon sides" do
+		expect{Polygon.build(0,1,1,1)}.to raise_error "Non-positive sides arent allowed"
 	end
 	it "Knows its perimeter" do
-		expect(Quadrilateral.build(1,1,1,1).perimeter).to eq(4)
-		expect(Quadrilateral.build(1,1,1,2).perimeter).to eq(5)
+		expect(Polygon.build(1,1,1,1).perimeter).to eq(4)
+		expect(Polygon.build(1,1,1,2).perimeter).to eq(5)
 	end
 	it "Knows if its a square" do
-		expect(Quadrilateral.build(1,1,1,1).is_square).to be(true)
-		expect(Quadrilateral.build(1,1,1,2).is_square).to be(false)
+		expect(Polygon.build(1,1,1,1).is_square).to be(true)
+		expect(Polygon.build(1,1,1,2).is_square).to be(false)
 	end
 	it "Knows if its a rectangle" do
-		expect(Quadrilateral.build(1,1,1,1).is_rectangle).to be(false)
-		expect(Quadrilateral.build(1,2,1,1).is_rectangle).to be(false)
-		expect(Quadrilateral.build(1,2,2,1).is_rectangle).to be(true)
-		expect(Quadrilateral.build(1,2,2,3).is_rectangle).to be(false)
+		expect(Polygon.build(1,1,1,1).is_rectangle).to be(false)
+		expect(Polygon.build(1,2,1,1).is_rectangle).to be(false)
+		expect(Polygon.build(1,2,2,1).is_rectangle).to be(true)
+		expect(Polygon.build(1,2,2,3).is_rectangle).to be(false)
 	end
 	it "Knows Square area" do
-		expect(Quadrilateral.build(1,1,1,1).area).to eq(1)
-		expect(Quadrilateral.build(2,2,2,2).area).to eq(4)
+		expect(Polygon.build(1,1,1,1).area).to eq(1)
+		expect(Polygon.build(2,2,2,2).area).to eq(4)
 	end
 	it "Knows rectangle area" do
-		expect(Quadrilateral.build(1,1,2,2).area).to eq(2)
-		expect(Quadrilateral.build(2,2,3,3).area).to eq(6)
+		expect(Polygon.build(1,1,2,2).area).to eq(2)
+		expect(Polygon.build(2,2,3,3).area).to eq(6)
 	end
 end
